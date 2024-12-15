f422340730d56c989089ecef4857f1bbddea5fa2
jbengler <broder.engler@gmail.com>
2024-12-15 23:21:14 +0100

    Update color schemes

--- R/tidycolor.R
+++ R/tidycolor.R
@@ -56,12 +56,6 @@ new_color_scheme <- function(x, name = "Untitled color scheme", reverse = FALSE)
 #' print(colors_discrete_seaside, return_html = TRUE)
 #' ```
 #' ```{r eval=FALSE}
-#' colors_discrete_alger
-#' ```
-#' ```{r echo=FALSE}
-#' print(colors_discrete_alger, return_html = TRUE)
-#' ```
-#' ```{r eval=FALSE}
 #' colors_discrete_apple
 #' ```
 #' ```{r echo=FALSE}
@@ -97,6 +91,12 @@ new_color_scheme <- function(x, name = "Untitled color scheme", reverse = FALSE)
 #' ```{r echo=FALSE}
 #' print(colors_discrete_candy, return_html = TRUE)
 #' ```
+#' ```{r eval=FALSE}
+#' colors_discrete_alger
+#' ```
+#' ```{r echo=FALSE}
+#' print(colors_discrete_alger, return_html = TRUE)
+#' ```
 #'
 #' @md
 #' @export
@@ -107,9 +107,6 @@ colors_discrete_friendly <- new_color_scheme(c("#0072B2","#56B4E9","#009E73","#F
 #' @rdname colors_discrete_friendly
 #' @export
 colors_discrete_seaside <- new_color_scheme(c("#8ecae6", "#219ebc", "#023047", "#ffb703", "#fb8500"), "colors_discrete_seaside")
-#' @rdname colors_discrete_friendly
-#' @export
-colors_discrete_alger <- new_color_scheme(c("#000000", "#1A5B5B", "#ACC8BE", "#F4AB5C", "#D1422F"), "colors_discrete_alger")
 #' @rdname colors_discrete_friendly
 #' @export
 colors_discrete_apple <- new_color_scheme(c("#ff3b30", "#ff9500", "#ffcc00", "#4cd964", "#5ac8fa", "#007aff", "#5856d6"), "colors_discrete_apple")
@@ -128,6 +125,10 @@ colors_discrete_metro <- new_color_scheme(c("#4DACD6","#4FAE62","#F6C54D","#E37D
 #' @rdname colors_discrete_friendly
 #' @export
 colors_discrete_candy <- new_color_scheme(c("#9b5de5", "#f15bb5", "#fee440", "#00bbf9", "#00f5d4"), "colors_discrete_candy")
+#' @rdname colors_discrete_friendly
+#' @export
+colors_discrete_alger <- new_color_scheme(c("#000000", "#1A5B5B", "#ACC8BE", "#F4AB5C", "#D1422F"), "colors_discrete_alger")
+
 
 
 #' Continuous color schemes
